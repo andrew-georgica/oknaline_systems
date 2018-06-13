07139cc0592863a6e0255bd7c21ee0078055fe3f
Andrii Georgica <georgica.andrii@gmail.com>
2018-06-13 17:57:09 +0300

    Project settings

--- js/side_display.js
+++ js/side_display.js
@@ -1,10 +1,12 @@
 
 left_block = document.getElementById("left_block");
+right_block = document.getElementById("right_block");
 
 var myScrollFunc = function () {
     var y = window.scrollY;
     if (y >= 150) {
-        left_block.className = "side_block show_left"
+        left_block.className = "side_block_left show_left";
+        right_block.className = "side_block_right show_right";
     } 
 };
 
