b5981940e067af62bfb70243b264fa98b3381557
Andrii Georgica <georgica.andrii@gmail.com>
2018-06-14 18:44:58 +0300

    Add Product section

--- js/side_display.js
+++ js/side_display.js
@@ -1,11 +1,13 @@
 
 left_block = document.getElementById("left_block");
 right_block = document.getElementById("right_block");
+title_container = document.getElementById("title_container");
 
 var myScrollFunc = function () {
     var y = window.scrollY;
-    if (y >= 150) {
+    if (y >= 250) {
         left_block.className = "side_block_left show_left";
+        title_container.className = "title_container show_left";
         right_block.className = "side_block_right show_right";
     } 
 };
