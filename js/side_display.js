b7c31e6e528a1caedb2a84625188c7dc6fb37851
Andrii Georgica <georgica.andrii@gmail.com>
2018-06-18 18:24:07 +0300

    2 new section & carousel

--- js/side_display.js
+++ js/side_display.js
@@ -2,9 +2,11 @@
 left_block = document.getElementById("left_block");
 right_block = document.getElementById("right_block");
 left_block_bottom = document.getElementById("left_block_bottom");
+advantages_left = document.getElementById("advantages_left");
 title_container_1 = document.getElementById("title_container_1");
 title_container_2 = document.getElementById("title_container_2");
 title_container_3 = document.getElementById("title_container_3");
+title_container_4 = document.getElementById("title_container_4");
 
 var myScrollFunc = function () {
     var y = window.scrollY;
@@ -13,14 +15,19 @@ var myScrollFunc = function () {
         title_container_1.className = "title_container show_left";
     }
     
-    if (y >= 1100 ) {
+    if (y >= 1000 ) {
         title_container_2.className = "title_container show_left";
         right_block.className = "side_block_right show_right";
     }
 
-    if (y >= 2100 ) {
+    if (y >= 1900 ) {
         left_block_bottom.className = "side_block_left show_left";
         title_container_3.className = "title_container show_left";
+    }
+
+    if (y >= 3200 ) {
+        advantages_left.className = "straight_side_block_left show_left";
+        title_container_4.className = "title_container_right show_right";
     }
 };
 
