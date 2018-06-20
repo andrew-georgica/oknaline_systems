de888ef64b78e18d988f5b4640d0bda64ead58b4
Andrii Georgica <georgica.andrii@gmail.com>
2018-06-20 18:20:09 +0300

    Add menu and buttons

--- js/side_display.js
+++ js/side_display.js
@@ -1,4 +1,5 @@
 
+header_block = document.getElementById("header_side_block");
 left_block = document.getElementById("left_block");
 right_block = document.getElementById("right_block");
 left_block_bottom = document.getElementById("left_block_bottom");
@@ -10,25 +11,31 @@ title_container_4 = document.getElementById("title_container_4");
 
 var myScrollFunc = function () {
     var y = window.scrollY;
-    if (y >= 250) {
+    
+
+    if (y >= 100) {
         left_block.className = "side_block_left show_left";
         title_container_1.className = "title_container show_left";
     }
     
-    if (y >= 1000 ) {
+    if (y >= 900 ) {
         title_container_2.className = "title_container show_left";
         right_block.className = "side_block_right show_right";
     }
 
-    if (y >= 1900 ) {
+    if (y >= 1850 ) {
         left_block_bottom.className = "side_block_left show_left";
         title_container_3.className = "title_container show_left";
     }
 
-    if (y >= 3200 ) {
+    if (y >= 3550 ) {
         advantages_left.className = "straight_side_block_left show_left";
         title_container_4.className = "title_container_right show_right";
     }
 };
 
 window.addEventListener("scroll", myScrollFunc);
+
+$(document).ready(function() {
+    header_block.className = "header_side_block show_right";
+});
